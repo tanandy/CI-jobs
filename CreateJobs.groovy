cf8feaac40cd18fe0935af2619e10200f949e21f
Antoine DUPRAT <aduprat@linagora.com>
2020-08-31 10:43:37 +0200

    Introduce seed job

---
+++ CreateJobs.groovy
@@ -0,0 +1,43 @@
+def repositories = [
+  'esn-api-client',
+  'esn-dav-import-client',
+  'esn-frontend-account-inbox',
+  'esn-frontend-account',
+  'esn-frontend-admin',
+  'esn-frontend-calendar',
+  'esn-frontend-common-libs',
+  'esn-frontend-contacts',
+  'esn-frontend-inbox',
+  'esn-frontend-inbox-calendar',
+  'esn-frontend-inbox-linshare',
+  'esn-frontend-linshare',
+  'esn-frontend-mailto',
+  'esn-frontend-videoconference-calendar'
+]
+
+repositories.each {
+  def repo = it
+
+  multibranchPipelineJob(repo) {
+    branchSources {
+      github {
+        id(repo)
+        scanCredentialsId('github')
+        repoOwner('OpenPaaS-Suite')
+        repository(repo)
+        buildOriginPRHead(true)
+        buildOriginBranchWithPR(false)
+      }
+    }
+    triggers {
+      periodicFolderTrigger {
+        interval('1')
+      }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+  }
+}
