e583ad6a430c7abbad60d08ca3673d99f204acaa
Andy Tan <18187444+tanandy@users.noreply.github.com>
2021-04-26 16:10:26 +0200

    Update CreateJobs.groovy

--- CreateJobs.groovy
+++ CreateJobs.groovy
@@ -40,6 +40,10 @@ repositories.each {
         }
       }
     }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+    }
     triggers {
       periodicFolderTrigger {
         interval('1')
