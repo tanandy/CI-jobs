7d97f9075cc3d41a04cc683dea49a12f5aa58927
Andy Tan <18187444+tanandy@users.noreply.github.com>
2021-04-27 14:04:03 +0200

    Update CreateJobs.groovy

--- CreateJobs.groovy
+++ CreateJobs.groovy
@@ -27,31 +27,53 @@ def repositories = [
 repositories.each {
   def repo = it
 
-  multibranchPipelineJob(repo) {
+  multibranchPipelineJob("Tmail build") {
     branchSources {
-      github {
-        id(repo)
-        scanCredentialsId('github')
-        repoOwner('OpenPaaS-Suite')
-        repository(repo)
-        buildOriginPRHead(true)
-        buildOriginBranchWithPR(false)
-        traits {
-          gitBranchDiscovery()
-          gitTagDiscovery()
+        branchSource {
+            source {
+                github {
+                    id(repo)
+                    credentialsId('github')
+                    repoOwner('OpenPaaS-Suite')
+                    repository(repo)
+                    repositoryUrl('')
+                    configuredByUrl(false)
+                    traits {
+                        gitHubTagDiscovery()
+                        gitHubBranchDiscovery { strategyId(1) }
+                        gitHubPullRequestDiscovery { strategyId(1) }
+                    }
+                }
+            }
+            buildStrategies {
+                buildAnyBranches {
+                    buildChangeRequests {
+                        ignoreTargetOnlyChanges(true)
+                        ignoreUntrustedChanges(false)
+                    }
+                    buildNamedBranches {
+                        filters {
+                            exact {
+                                name('master')
+                                caseSensitive(true)
+                            }
+                        }
+                    }
+                    buildTags {
+                        atLeastDays '-1'
+                        atMostDays '14'
+                    }
+                }
+            }
         }
-      }
     }
-	configure {
-      def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
-      
-      traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
-        strategyId(1)
-      }
-      traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
-        strategyId(1)
-      }
-      traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait'()
+    configure {
+        // workaround for JENKINS-46202 (https://issues.jenkins-ci.org/browse/JENKINS-46202)
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait' {
+            strategyId 1
+            trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustPermission')
+        }
     }
     triggers {
       periodicFolderTrigger {
